d5b79da1976ca5e2a0ce289caf19aa2a173b5ec6
Karan Patel <karan2605@hotmail.co.uk>
2022-11-06 11:37:47 +0000

    List the 3 properties

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -42,10 +42,17 @@ async function main() {
     await transaction.wait()
   }
 
+  // Listing properties...
+  transaction = await escrow.connect(seller).list(1, buyer.address, tokens(20), tokens(10))
+  await transaction.wait()
 
+  transaction = await escrow.connect(seller).list(2, buyer.address, tokens(15), tokens(5))
+  await transaction.wait()
 
+  transaction = await escrow.connect(seller).list(3, buyer.address, tokens(10), tokens(5))
+  await transaction.wait()
 
-
+  console.log(`Finished.`)
 
 }
 
